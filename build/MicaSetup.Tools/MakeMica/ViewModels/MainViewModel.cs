4d8baac2f5283b779a85284e68a8a8a2764e96a4
ema <mccoy39082@163.com>
2024-06-10 07:41:40 +0800

    update: MakeMica simple layout

--- build/MicaSetup.Tools/MakeMica/ViewModels/MainViewModel.cs
+++ build/MicaSetup.Tools/MakeMica/ViewModels/MainViewModel.cs
@@ -1,10 +1,50 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using MakeMica.Design.Helpers;
+using System.Windows;
 
 namespace MakeMica.ViewModels;
 
 public partial class MainViewModel : ObservableRecipient
 {
+    [ObservableProperty]
+    private string code = string.Empty;
+
+    public MainViewModel()
+    {
+        Code = ResourceHelper.GetString(@"pack://application:,,,/Resources/Templates/Sample.json");
+    }
+
+    [RelayCommand]
+    private void New()
+    {
+        // TODO
+    }
+
+    [RelayCommand]
+    private void Open()
+    {
+        // TODO
+    }
+
+    [RelayCommand]
+    private void Save()
+    {
+        // TODO
+    }
+
+    [RelayCommand]
+    private void SaveAs()
+    {
+        // TODO
+    }
+
+    [RelayCommand]
+    private void Exit()
+    {
+        Application.Current.Shutdown();
+    }
+
     [RelayCommand]
     private void Build()
     {
@@ -13,6 +53,12 @@ public partial class MainViewModel : ObservableRecipient
 
     [RelayCommand]
     private void OpenFolder()
+    {
+        // TODO
+    }
+
+    [RelayCommand]
+    private void Beautify()
     {
         // TODO
     }
