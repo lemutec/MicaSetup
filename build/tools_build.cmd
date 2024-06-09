d99018c6f19554f90f5031c3f999bac0919d6908
ema <mccoy39082@163.com>
2024-06-10 06:34:43 +0800

    new: tools build script

---
+++ build/tools_build.cmd
@@ -0,0 +1,25 @@
+cd /d %~dp0
+
+@echo [prepare somethings]
+del MicaSetup.exe
+for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath`) do set "path=%path%;%%i\MSBuild\Current\Bin;%%i\Common7\IDE"
+
+echo [build app using vs2022]
+cd ..\src\
+dotnet publish -c Release -p:PublishProfile=FolderProfile
+cd /d %~dp0
+
+@echo [finish]
+mkdir .\Build\
+mkdir .\Build\bin\
+
+copy /y .\MicaSetup.Tools\MakeMica\bin\Release\publish\MakeMica.exe .\Build\
+ren .\Build\MakeMica.exe makemicaw.exe
+
+copy /y .\MicaSetup.Tools\MicaSquircle\bin\Release\publish\MicaSquircle.exe .\Build\
+ren .\Build\MicaSquircle.exe makeiconw.exe
+
+copy /y .\MicaSetup.Tools\7-Zip\7z.dll .\Build\bin\7z.dll
+copy /y .\MicaSetup.Tools\7-Zip\7z.exe .\Build\bin\7z.exe
+
+@pause
