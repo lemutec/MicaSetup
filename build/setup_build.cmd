0c435033f79a23e3eb86355b05b1a9a266f3098a
ema <mccoy39082@163.com>
2024-06-10 05:35:08 +0800

    change: AppendTargetFrameworkToOutputPath false

--- build/setup_build.cmd
+++ build/setup_build.cmd
@@ -10,17 +10,17 @@ dotnet publish -c Release -p:PublishProfile=FolderProfile
 cd /d %~dp0
 
 echo [pack app using 7z]
-MicaSetup.Tools\7-Zip\7z a publish.7z ..\src\MicaApp\bin\Release\net472\publish\* -t7z -mx=5 -mf=BCJ2 -r -y
+MicaSetup.Tools\7-Zip\7z a publish.7z ..\src\MicaApp\bin\Release\publish\* -t7z -mx=5 -mf=BCJ2 -r -y
 copy /y publish.7z .\MicaSetup\Resources\Setups\publish.7z
 
 @echo [build uninst using vs2022]
 msbuild MicaSetup\MicaSetup.Uninst.csproj /t:Rebuild /p:Configuration=Release /p:DeployOnBuild=true /p:PublishProfile=FolderProfile /restore
 
 @echo [build setup using vs2022]
-copy /y .\MicaSetup\bin\Release\net472\MicaSetup.exe .\MicaSetup\Resources\Setups\Uninst.exe
+copy /y .\MicaSetup\bin\Release\MicaSetup.exe .\MicaSetup\Resources\Setups\Uninst.exe
 msbuild MicaSetup\MicaSetup.csproj /t:Build /p:Configuration=Release /p:DeployOnBuild=true /p:PublishProfile=FolderProfile /restore
 
 @echo [finish]
-copy /y .\MicaSetup\bin\Release\net472\MicaSetup.exe .\
+copy /y .\MicaSetup\bin\Release\MicaSetup.exe .\
 
 @pause
