0104315361e7a85f5be36602a2dac0f9548d6854
ema <mccoy39082@163.com>
2024-06-10 03:50:27 +0800

    update: MicaSquircle include ttf file

--- build/MicaSetup.Tools/MicaSquircle/Core/PrivateFontHelper.cs
+++ build/MicaSetup.Tools/MicaSquircle/Core/PrivateFontHelper.cs
@@ -2,7 +2,10 @@
 using System.Drawing;
 using System.Drawing.Text;
 using System.IO;
+using System.IO.Packaging;
 using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Resources;
 
 namespace MicaSquircle.Core;
 
@@ -13,11 +16,21 @@ internal static class PrivateFontHelper
 
     static PrivateFontHelper()
     {
-        string fontFilePath = Environment.CurrentDirectory + @"\..\..\..\..\..\..\build\MicaSetup\Resources\Fonts\HarmonyOS_Icons_Slim.ttf";
-        byte[] fontData = File.ReadAllBytes(fontFilePath);
+        if (!UriParser.IsKnownScheme("pack"))
+            _ = PackUriHelper.UriSchemePack;
+
+        byte[] fontData = GetBytes("pack://application:,,,/MicaSquircle;component/Resources/Fonts/HarmonyOS_Icons_Slim.ttf");
         nint fontPtr = Marshal.AllocCoTaskMem(fontData.Length);
         Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
         PrivateFontCollection.AddMemoryFont(fontPtr, fontData.Length);
         Marshal.FreeCoTaskMem(fontPtr);
+    }
+
+    public static byte[] GetBytes(string uriString)
+    {
+        Uri uri = new(uriString);
+        StreamResourceInfo info = Application.GetResourceStream(uri);
+        using BinaryReader stream = new(info.Stream);
+        return stream.ReadBytes((int)info.Stream.Length);
     }
 }
