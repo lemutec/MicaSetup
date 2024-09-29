3b5a93d32f0dfce6abc739b79e7656b293c266e9
ema <mccoy39082@163.com>
2024-09-29 11:17:59 +0800

    change: simplify version getter

--- build/MicaSetup.Tools/MakeIcon/ViewModels/AboutViewModel.cs
+++ build/MicaSetup.Tools/MakeIcon/ViewModels/AboutViewModel.cs
@@ -1,29 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
 using System.Reflection;
-using System.Text;
 
 namespace MakeIcon.ViewModels;
 
 public partial class AboutViewModel : ObservableObject
 {
     [ObservableProperty]
-    private string version = $"v{Assembly.GetCallingAssembly().GetAssemblyVersion()}";
-}
-
-file static class AssemblyExtensions
-{
-    public static string GetAssemblyVersion(this Assembly assembly)
-    {
-        Version version = assembly.GetName().Version;
-        StringBuilder sb = new();
-
-        sb.Append(version.Major);
-        sb.Append(".");
-        sb.Append(version.Minor);
-        sb.Append(".");
-        sb.Append(version.Build);
-
-        return sb.ToString();
-    }
+    private string version = $"v{Assembly.GetCallingAssembly().GetName().Version.ToString(3)}";
 }
