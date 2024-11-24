b957730e1d71b86fa099d925f7e33ff08b0bde71
ema <mccoy39082@163.com>
2024-11-24 20:44:12 +0800

    chore: build template script

---
+++ build/MicaSetup/README.md
@@ -0,0 +1,9 @@
+# MicaSetup
+
+Default template.
+
+Github: https://github.com/lemutec/MicaSetup
+
+### License
+
+https://github.com/lemutec/MicaSetup/blob/v2/LICENSE
---
+++ build/template_build.cmd
@@ -0,0 +1,10 @@
+rd /s /q .\MicaSetup\obj
+rd /s /q .\MicaSetup\bin
+del .\MicaSetup\bin\MicaSetup.csproj.user
+del .\MicaSetup\Resources\Setups\publish.7z
+del .\MicaSetup\Resources\Setups\publish.cer
+del .\MicaSetup\Resources\Setups\Uninst.exe
+MicaSetup.Tools\7-Zip\7z a default.7z .\MicaSetup\* -t7z -mx=5 -mf=BCJ2 -r -y
+mkdir .\Build\template
+move default.7z .\Build\template\default.7z
+@pause
