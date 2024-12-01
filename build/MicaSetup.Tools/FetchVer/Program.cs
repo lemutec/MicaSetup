ff88962cd86e895eb85f7d95eb9f2b1fa23d9f97
ema <mccoy39082@163.com>
2024-12-02 01:27:37 +0800

    feat: prepare new tool named fetchver

---
+++ build/MicaSetup.Tools/FetchVer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace FetchVer;
+
+internal sealed class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"Startup: fetchver v{Assembly.GetCallingAssembly().GetName().Version.ToString(3)}");
+
+        if (args.Length <= 0)
+        {
+            // Support exe/dll/csproj/cs files
+            Console.WriteLine("Usage: fetchver \"path/to/exe\"");
+
+#if DEBUG
+            args = ["FetchVersion.exe"];
+#endif
+        }
+    }
+}
