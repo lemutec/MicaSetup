9624672d40e8ecdf806a2aab15ea7cdb9c25fb22
ema <mccoy39082@163.com>
2026-08-19 19:42:20 +0800

    Bump version

--- build/MicaSetup/Program.cs
+++ build/MicaSetup/Program.cs
@@ -13,8 +13,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("MicaApp Setup")]
 [assembly: AssemblyCompany("Lemutec")]
 [assembly: AssemblyCopyright("Under MIT License. Copyright (c) Lemutec Contributors.")]
-[assembly: AssemblyVersion("2.5.4.0")]
-[assembly: AssemblyFileVersion("2.5.4.0")]
+[assembly: AssemblyVersion("2.5.5.0")]
+[assembly: AssemblyFileVersion("2.5.5.0")]
 [assembly: RequestExecutionLevel("admin")]
 
 namespace MicaSetup;
@@ -65,7 +65,7 @@ internal class Program
                 option.ExeName = "MicaApp.exe";
                 option.DisplayName = $"{option.AppName}";
                 option.DisplayIcon = $"{option.ExeName}";
-                option.DisplayVersion = "2.5.4.0";
+                option.DisplayVersion = "2.5.5.0";
                 option.Publisher = "Lemutec";
                 option.SetupName = $"{option.AppName} {"Setup".Tr()}";
                 option.MessageOfPage1 = $"{option.AppName}";
